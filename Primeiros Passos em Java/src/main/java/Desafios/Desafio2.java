39e494021169097805d701169129d7b169b5652a
RBANDEIRA72 <rogermendesbandeira@gmail.com>
2021-08-02 20:25:00 -0300

    Adicionando Desafios Java Bootcamp Santander e Everis

--- Primeiros Passos em Java/src/main/java/Desafios/Desafio2.java
+++ Primeiros Passos em Java/src/main/java/Desafios/Desafio2.java
@@ -1,8 +1,29 @@
 package Desafios;
 
+/*
+Leia um valor inteiro X. Em seguida apresente os 6 valores ímpares
+consecutivos a partir de X, um valor por linha, inclusive o X se for o caso.
+
+Entrada
+A entrada será um valor inteiro positivo.
+
+Saída
+A saída será uma sequência de seis números ímpares.
+
+
+Exemplo de Entrada	Exemplo de Saída
+8
+
+9
+11
+13
+15
+17
+19
+*/
+
 import java.io.IOException;
 import java.util.Scanner;
-
 
 public class Desafio2 {
 
--- Primeiros Passos em Java/src/main/java/Desafios/Desafio3.java
+++ Primeiros Passos em Java/src/main/java/Desafios/Desafio3.java
@@ -1,5 +1,31 @@
 package Desafios;
 
+/*
+Desafio
+Crie um programa que leia 6 valores. Você poderá receber valores negativos
+e/ou positivos como entrada, devendo desconsiderar os valores nulos.
+Em seguida, apresente a quantidade de valores positivos digitados.
+
+Entrada
+Você receberá seis valores, negativos e/ou positivos.
+
+Saída
+Exiba uma mensagem dizendo quantos valores positivos foram lidos assim
+como é exibido abaixo no exemplo de saída. Não esqueça da mensagem
+"valores positivos" ao final.
+
+
+Exemplo de Entrada	Exemplo de Saída
+7
+-5
+6
+-3.4
+4.6
+12
+
+4 valores positivos
+*/
+
 import java.util.Locale;
 import java.util.Scanner;
 
