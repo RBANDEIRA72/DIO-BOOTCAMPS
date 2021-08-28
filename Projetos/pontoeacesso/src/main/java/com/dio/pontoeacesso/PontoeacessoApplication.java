f22a1fd6e10c8294f3df14794df537171a1dd18c
RBANDEIRA72 <rogermendesbandeira@gmail.com>
2021-08-27 22:23:32 -0300

    Atualizando andamento do Bootcamp

---
+++ Projetos/pontoeacesso/src/main/java/com/dio/pontoeacesso/PontoeacessoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.pontoeacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class PontoeacessoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PontoeacessoApplication.class, args);
+	}
+}
---
+++ Projetos/pontoeacesso/src/test/java/com/dio/pontoeacesso/PontoeacessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.pontoeacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoeacessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
